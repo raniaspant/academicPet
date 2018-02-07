89f0573104e5346dfe3116a646ff5f876d896b60
raniaspant <rania.spantidi@gmail.com>
2018-02-07 17:11:21 -0600

    Transition from menu script

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject flashText;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("flashTheText", 0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+            SceneManager.LoadScene("Game");
+	}
+
+    void flashTheText()
+    {
+        if (flashText.activeInHierarchy)
+            flashText.SetActive(false);
+        else
+            flashText.SetActive(true);
+    }
+}

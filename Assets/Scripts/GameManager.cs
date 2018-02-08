e356390a9e6c4e711b54ce0b33673c0045521945
raniaspant <rania.spantidi@gmail.com>
2018-02-07 20:38:09 -0600

    Using physics instead of UI for ground

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject happinessText;
+    public GameObject hungerText;
+
+    public GameObject robot;
+
+
+	void Update () {
+        happinessText.GetComponent<Text>().text = robot.GetComponent<Robot>().happiness.ToString();
+        hungerText.GetComponent<Text>().text = robot.GetComponent<Robot>().hunger.ToString();
+    }
+}
---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Robot : MonoBehaviour {
+
+    [SerializeField]
+    private int _hunger;
+    [SerializeField]
+    private int _happiness;
+
+    private bool _serverTime;
+
+	// Use this for initialization
+	void Start () {
+        updateStatus();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void updateStatus()
+    {
+        if (!PlayerPrefs.HasKey("_hunger"))
+        {
+            _hunger = 100;
+            PlayerPrefs.SetInt("_hunger", _hunger);
+        }else
+        {
+            _hunger = PlayerPrefs.GetInt("_hunger");
+            // TODO update the hunger and stuff
+        }
+        if (!PlayerPrefs.HasKey("_happiness"))
+        {
+            _happiness = 100;
+            PlayerPrefs.SetInt("_happiness", _happiness);
+        }
+        else
+        {
+            _happiness = PlayerPrefs.GetInt("_happiness");
+            // TODO update the hunger and stuff
+        }
+        if (!PlayerPrefs.HasKey("then"))
+            PlayerPrefs.SetString("then", getStringTime());
+        if (_serverTime)
+            updateServer();
+        else
+            InvokeRepeating("updateDevice", 0f, 30f);
+    }
+
+    void updateServer()
+    {
+        PlayerPrefs.SetString("then", getStringTime());
+    }
+
+    void updateDevice()
+    {
+
+    }
+
+    TimeSpan getTimeSpan()
+    {
+        if (_serverTime)
+            return new TimeSpan();
+        else
+            return DateTime.Now - Convert.ToDateTime(PlayerPrefs.GetString("then"));
+    }
+
+    string getStringTime()
+    {
+        DateTime now = DateTime.Now;
+        return now.Month + "/" + now.Day + "/" + now.Year + " " + now.Hour + ":" + now.Minute + ":" + now.Second;
+    }
+
+    public int hunger
+    {
+        get { return _hunger; }
+        set { _hunger = value; }
+    }
+
+    public int happiness
+    {
+        get { return _happiness; }
+        set { _happiness = value; }
+    }
+}
